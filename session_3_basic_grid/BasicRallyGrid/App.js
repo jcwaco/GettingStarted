4f5a785f4b248ae5930d526d6e2d71086e2cf120
jcarroll <jcarroll@rallydev.com>
2014-05-03 15:02:57 -0500

    Changes for the Basic Grid. Added pulldowns and made Grid responsive to changes in the pulldowns.

--- session_3_basic_grid/BasicRallyGrid/App.js
+++ session_3_basic_grid/BasicRallyGrid/App.js
@@ -9,39 +9,121 @@ Ext.define('CustomApp', {
     // Entry Point to App
     launch: function() {
 
-      console.log('our first app');     // see console api: https://developers.google.com/chrome-developer-tools/docs/console-api
-      this._loadData();                 // we need to prefix with 'this.' so we call a method found at the app level.
+      console.log('our second app');     // see console api: https://developers.google.com/chrome-developer-tools/docs/console-api
+
+      this.pulldownContainer = Ext.create('Ext.container.Container', {
+    	  layout: {
+    	        type: 'hbox',
+    	        align: 'stretch'
+    	  },
+    	  
+      });
+      this.add(this.pulldownContainer);
+      
+      this._loadIterations();
+    },
+    
+    // Get list of Iterations from Rally
+    _loadIterations: function() {
+    
+    	this.iterationComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+    		fieldLabel: 'Iteration',
+    		labelAlign: 'right',
+    		width: 300,
+    		listeners: {
+    			ready: function(comboBox) {
+    				console.log('Ready!', comboBox);
+    				// var selectedIteration = comboBox.getRecord().get('_ref');
+    				// this._loadData();
+    				this._loadSeverities();
+    				
+    			},
+    			select: function(comboBox, records) {
+    				console.log('Ready!', comboBox);
+    				this._loadData();
+    			},
+        		scope: this
+    		}
+    	});
+    	this.pulldownContainer.add(this.iterationComboBox);
+    },
+    
+    _loadSeverities: function() {
+    	this.severityComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+    		model: 'Defect',
+    		field: 'Severity',
+    		fieldLabel: 'Defect Severity',
+    		labelAlign: 'right',
+    		listeners: {
+    			ready: function (fvComboBox) {
+    				this._loadData();
+    			},
+    			select: function (fvComboBox, records) {
+    				this._loadData();
+    			},
+    			scope: this
+    		}
+    		
+    	});
+    	this.pulldownContainer.add(this.severityComboBox);
     },
 
     // Get data from Rally
     _loadData: function() {
-
-      var myStore = Ext.create('Rally.data.wsapi.Store', {
-          model: 'User Story',
-          autoLoad: true,                         // <----- Don't forget to set this to true! heh
+        var selectedIterationRef = this.iterationComboBox.getRecord().get('_ref');
+        var selectedSeverityValue = this.severityComboBox.getRecord().get('value');
+     console.log('Selected Iteration:', selectedIterationRef);
+     console.log('Selected Severity:', selectedSeverityValue);
+     
+     var myFilters =  [
+                           	{
+                           		property: 'Iteration', 
+                            	// operation: '=',
+                            	value: selectedIterationRef},
+                            	{
+                                property: 'Severity', 
+                                // operation: '=',
+                                value: selectedSeverityValue}
+                       ];
+     // If store exists just load new data
+     //
+     if (this.defectStore) {
+    	 console.log('Store exists');
+    	this.defectStore.setFilter(myFilters);
+     	this.defectStore.load();
+    } else{
+    	console.log('Store Does not exist!');
+      this.defectStore = Ext.create('Rally.data.wsapi.Store', {
+          model: 'Defect',
+          autoLoad: true,	// <----- Don't forget to set this to true! heh
+          filters: myFilters,
           listeners: {
               load: function(myStore, myData, success) {
                   console.log('got data!', myStore, myData);
-                  this._loadGrid(myStore);      // if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
-              },
+                  if (!this.myGrid) {
+                	  this._createGrid(myStore);      // if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
+                  					}
+                  },
               scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
           },
-          fetch: ['FormattedID', 'Name', 'ScheduleState']   // Look in the WSAPI docs online to see all fields available!
+          fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']   // Look in the WSAPI docs online to see all fields available!
         });
 
-    },
+    } 
+  },
 
     // Create and Show a Grid of given stories
-    _loadGrid: function(myStoryStore) {
+    _createGrid: function(myDefectStore) {
 
-      var myGrid = Ext.create('Rally.ui.grid.Grid', {
-        store: myStoryStore,
+    	console.log('Creating Grid!!!');
+    	this.myGrid = Ext.create('Rally.ui.grid.Grid', {
+        store: myDefectStore,
         columnCfgs: [         // Columns to display; must be the same names specified in the fetch: above in the wsapi data store
-          'FormattedID', 'Name', 'ScheduleState'
+          'FormattedID', 'Name', 'Severity', 'Iteration'
         ]
       });
 
-      this.add(myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
+      this.add(this.myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
 
       console.log('what is this?', this);
 
