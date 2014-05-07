8b566b8ef0919de5ef16c102fb8af98ffa9f9b6b
jcarroll <jcarroll@rallydev.com>
2014-05-07 14:24:26 -0500

    This change addresses DE17. Fixed

--- session_3_basic_grid/BasicRallyGrid/App.js
+++ session_3_basic_grid/BasicRallyGrid/App.js
@@ -1,6 +1,8 @@
 //Custom Rally App that displays Stories in a grid.
 
 //Note: various console debugging messages intentionally kept in the code for learning purposes
+// This change fixes Defect 17
+//
 Ext.define('CustomApp', {
 	extend : 'Rally.app.App', // The parent class manages the app 'lifecycle' and calls launch() when ready
 	componentCls : 'app', // CSS styles found in app.css
